7c71f06cc73ffa6ec3bb013c019197cb2c4906b0
Mohit-d13 <mohitdamle14@gmail.com>
2026-07-02 01:34:56 +0530

    deployment chart, cluster config and authentication

--- core/config.py
+++ core/config.py
@@ -1,3 +1,4 @@
+import os
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings
@@ -8,10 +9,24 @@ class RedisSettings(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
+    PASSWORD_FILE_PATH: str = "./secret/redis-creds/password"
+
+    @property
+    def REDIS_PASSWORD(self) -> str:
+        if not os.path.exists(self.PASSWORD_FILE_PATH):
+            raise ValueError(f"Password file path not found {self.PASSWORD_FILE_PATH}")
+        with open(self.PASSWORD_FILE_PATH) as f:
+            password = f.read().strip()
+        if not password:
+            raise ValueError("Redis password file is empty")
+        return password
 
     @property
     def REDIS_URL(self) -> str:
-        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+        return (
+            f"redis://:{self.REDIS_PASSWORD}"
+            f"@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+        )
 
 
 @lru_cache
